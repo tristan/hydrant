060f24f7802ad79961f35c511beae18a639f1e4f
jc124742 <jc124742@08480534-fc8c-4714-a454-f8bfd68e69a9>
2008-02-27 01:37:51 +0000

    make / redirect to /portal/
    git-svn-id: https://www.hpc.jcu.edu.au/projects/kepler/svn/WebPortal/trunk@350 08480534-fc8c-4714-a454-f8bfd68e69a9

--- src/main/jython/urls.py
+++ src/main/jython/urls.py
@@ -1,12 +1,17 @@
 from django.conf.urls.defaults import *
+from django.http import HttpResponseRedirect
 from settings import MEDIA_ROOT, ROOT_URL
 
 urlpatterns = patterns('',
-    (r'^%smedia/workflows/(?P<path>.*)$' % ROOT_URL, 'kepler.portalviews.hide_workflows'),
-    (r'^%smedia/(?P<path>.*)$' % ROOT_URL, 'django.views.static.serve', {'document_root': MEDIA_ROOT, 'show_indexes': True}),
-    (r'^%saccounts/login/$' % ROOT_URL, 'django.contrib.auth.views.login', {'template_name': 'kepler/login.html'}, 'login_view'),
-    (r'^%saccounts/logout/$' % ROOT_URL, 'django.contrib.auth.views.logout', {'template_name': 'kepler/logout.html'}, 'logout_view'),
-    #(r'^kepler/', include('jython.kepler.urls')),
+    (r'^%smedia/workflows/(?P<path>.*)$' % ROOT_URL, 
+     'kepler.portalviews.hide_workflows'),
+    (r'^%smedia/(?P<path>.*)$' % ROOT_URL, 'django.views.static.serve', 
+     {'document_root': MEDIA_ROOT, 'show_indexes': True}),
+    (r'^%saccounts/login/$' % ROOT_URL, 'django.contrib.auth.views.login',
+     {'template_name': 'kepler/login.html'}, 'login_view'),
+    (r'^%saccounts/logout/$' % ROOT_URL, 'django.contrib.auth.views.logout',
+     {'template_name': 'kepler/logout.html'}, 'logout_view'),
     (r'^%sportal/' % ROOT_URL, include('jython.kepler.portalurls')),
+    (r'^%s' % ROOT_URL, lambda request: HttpResponseRedirect('/%sportal/' % ROOT_URL)),
     (r'^%sadmin/' % ROOT_URL, include('django.contrib.admin.urls')),
 )
