6a3e7fbd010fb8627d8ecdd015e76e41aa533d81
Tristan King <tristan.king@jcu.edu.au>
2008-05-06 16:30:55 +1000

    fixed bug where modified workflow parameters that aren't exposed weren't being set when a job starts

--- src/main/jython/hydrant/job/__init__.py
+++ src/main/jython/hydrant/job/__init__.py
@@ -1,17 +1,28 @@
 from helpers import *
+from hydrant.models import JobInput
 from execution import default_job_manager
 
 def queue_new_job(job):
     model = job.workflow.get_proxy_object(True)
     pe = model.proxied_entity
     pe.workspace().getWriteAccess()
+    params_set = []
     for input in job.get_job_inputs():
         id = input.parameter.property_id
+        params_set.append(input.parameter)
         il = id.split('.')[1:]
         path_to_actor = il[1:-1]
         prop = il[-1]
         print '(%s, %s, %s)' % (path_to_actor, prop, input.value)
         model.set_actor_property(path_to_actor, prop, input.value)
+    for param in job.workflow.get_all_parameters():
+        if param not in params_set:
+            id = param.property_id
+            il = id.split('.')[1:]
+            path_to_actor = il[1:-1]
+            prop = il[-1]
+            model.set_actor_property(path_to_actor, prop, param.value)
+            JobInput(job=job, parameter=param,value=param.value).save()
     drm = DefaultReplacementManager(pe, 'replacement-manager', job.pk)
     prov = ProvenanceListenerWrapper(pe, 'provenancelistener', job.pk)
     modify_rexpression_actors(pe, drm)
